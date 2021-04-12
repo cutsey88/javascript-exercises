1847f998ab49bcd29ea5378376ae3c82a556b473
cutsey88 <chrisautsey88@gmail.com>
2021-04-11 23:53:53 -0400

    Finish reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let output = ''
+    let i = string.length
+    while (i > 0) {
+        output += string[i - 1];
+        i--;
+    }
+    return output;
 }
 
 module.exports = reverseString
