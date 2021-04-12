635526e5112f12d2381571983eb468ae2b9577f4
cutsey88 <chrisautsey88@gmail.com>
2021-04-12 04:35:44 -0400

    Finish removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...cutVal) {
 
+    for (let i = 0; i < array.length; i++) {
+    
+        for (let j = 0; j < cutVal.length; j++) {
+            if (cutVal[j] === array[i]) {
+                array.splice(i, 1)
+                i -= i;
+            }
+        }
+    }
+    return array;
 }
 
 module.exports = removeFromArray
