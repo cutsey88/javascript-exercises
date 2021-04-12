7cf54dbac14bdf48e9dfd9cd2c48aec17dfce6f4
cutsey88 <chrisautsey88@gmail.com>
2021-04-12 05:45:53 -0400

    Finish tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5 / 9);
+  let roundCelsius = Number(celsius.toFixed(1));
+  return roundCelsius;
 }
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let fahrenheit = (celsius * 9 / 5) + 32;
+  let roundFahrenheit = Number(fahrenheit.toFixed(1));
+  return roundFahrenheit;
 }
 
 module.exports = {
