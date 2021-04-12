5ae690a98726ca6c21b7d2383a2c81ffb5fe97a2
cutsey88 <chrisautsey88@gmail.com>
2021-04-12 05:18:31 -0400

    Finish sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';   
+    } else if (typeof num1 != 'number' || typeof num2 != 'number') {
+        return 'ERROR'
+    } else {
+        if (num2 > num1) {
+            for (let counter = num1; counter <= num2; counter++) {
+                sum += counter;
+            }
+        } else if (num1 > num2) {
+            for (let counter = num2; counter <= num1; counter++) {
+                sum += counter;
+            }    
+        }
+    return sum;
+        
+    }
 }
 
 module.exports = sumAll
