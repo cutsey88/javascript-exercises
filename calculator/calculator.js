bad2fb23cfa26b9b3742fcd50d0061f3bee0c165
cutsey88 <chrisautsey88@gmail.com>
2021-05-15 11:07:17 -0400

    Add calculator add, subtract, sum, multiply, power, factorial

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,47 @@
-function add () {
-	
+function add (operand1, operand2) {
+	return operand1 + operand2;
 }
 
-function subtract () {
-	
+function subtract (operand1, operand2) {
+	return operand1 - operand2;
 }
 
-function sum () {
-	
+function sum (operands) {
+	let opSum = 0;
+	for (i = 0; i < operands.length; i++) {
+		opSum += operands[i];
+	}
+	return opSum;
 }
 
-function multiply () {
-	
+function multiply (operands) {
+	let opSum;
+	for (i = 0; i < operands.length; i++) {
+		if (!opSum) {
+			opSum = operands[i];
+			} else {
+				opSum *= operands[i];
+			}
+	}
+	return opSum;
 }
 
-function power() {
-	
+function power(base, exponent) {
+	if (exponent >= 0) {
+		if (exponent === 0) {
+			return 1;
+		} else {
+			return base * power(base, exponent -1);
+		}
+	}
 }
 
-function factorial() {
-	
+function factorial(operand) {
+	if (operand === 0 || operand === 1) {
+		return 1;
+	} else {
+		return operand * factorial(operand - 1);
+	};
 }
 
 module.exports = {
