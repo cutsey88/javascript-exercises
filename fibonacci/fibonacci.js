4d9b82541ec1d56bc07bc65b92390141cefd8c25
cutsey88 <chrisautsey88@gmail.com>
2021-05-16 02:52:49 -0400

    Finish fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(place) {
+    if (place < 0) {
+        return 'OOPS';
+    }
+    if (place == 1 || place == 2) {
+        return 1;
+    }
+    let fiboArray = [];
+    for (i = 0; i < place; i++) {
+        if (i == 0 || i == 1) {
+            fiboArray[i] = 1;
+        } else {
+            fiboArray[i] = fiboArray[i - 1] + fiboArray[i - 2];
+        }
+    }
+    return fiboArray[place - 1];
 }
 
 module.exports = fibonacci
