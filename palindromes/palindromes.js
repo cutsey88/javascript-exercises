f5ee5e0d84ca96108b457a567ae023c3bf5208b0
cutsey88 <chrisautsey88@gmail.com>
2021-05-16 02:32:53 -0400

    Finish palindrome

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    let reverse = '';
+    let checkString = string;
+    let letters = /^[A-Za-z]+$/;
+    
+    for (i = 1; i <= string.length; i++) {
+        if (!string[string.length -i].match(letters)) {
+            checkString = checkString.slice(0,string.length - i) + checkString.slice(string.length - i + 1, checkString.length);
+            continue;
+        } else {
+            reverse += string[string.length - i];
+        }
+    }
+    return checkString.toLowerCase() === reverse.toLowerCase();
 }
 
 module.exports = palindromes
